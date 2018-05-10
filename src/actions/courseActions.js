a84e7e2af19d117b62d3d70f1c38ac3941ee28f5
jeradcliffe <jacobradcliffe2012@gmail.com>
2018-05-10 10:43:54 -0500

    Added mock api

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,5 @@
+import {CREATE_COURSE} from '../actions/actionTypes';
+
 export function createCourse(course) {
-    return {type: 'CREATE_COURSE', course}
+    return {type: CREATE_COURSE, course}
 }
